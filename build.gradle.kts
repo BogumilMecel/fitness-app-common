790ec3a62fb79eb1619bc166a2b7e0ddc78990cb
Bogumil Mecel <bogumilmecel2@gmail.com>
2024-03-23 14:52:02 +0100

    1.7 version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,53 @@
 plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times
-    // in each subproject's classloader
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.multiplatform)
+    id("maven-publish")
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "17"
+            }
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "main"
+            isStatic = true
+        }
+    }
+
+    sourceSets.commonMain.dependencies {
+        api(project(":auth"))
+        api(project(":diary"))
+        api(project(":shared"))
+        api(project(":introduction"))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.bodzio6978"
+            artifactId = "fitness-app-common"
+            version = "1.7"
+        }
+    }
+}
+
+android {
+    namespace = "com.gmail.bogumilmecel2.main"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
